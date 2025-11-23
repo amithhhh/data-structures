877c59d15fb8e1b23297eea4eb52b6e5ecf5951f
Amith <amithajith7025@gmail.com>
2025-11-23 11:07:53 +0530

    Learnt how to connect nodes

---
+++ Linked-List/exp2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+	int data;
+	struct Node *next;
+};
+
+int main() {
+	struct Node *first = (struct Node*) malloc(sizeof(struct Node));
+	struct Node *second = (struct Node*) malloc(sizeof(struct Node));
+
+	first->data = 10;
+	first->next = second;
+
+	second->data = 20;
+	second->next = NULL;
+
+	struct Node *temp = first;
+
+	while(temp != NULL) {
+		printf("%d\t", temp->data);
+		temp = temp->next;
+	}
+	printf("\n");
+	return 0;
+}
