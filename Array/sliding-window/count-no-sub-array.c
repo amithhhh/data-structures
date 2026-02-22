fd07ecc7a5e95b411ad0c134612b1dfe0dc43096
amithhhhh <amithajith7025@gmail.com>
2026-02-22 22:16:08 +0530

    completed count no of sub array with size k

--- Array/sliding-window/count-no-sub-array.c
+++ Array/sliding-window/count-no-sub-array.c
@@ -2,26 +2,84 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
+struct Node {
+	int key;
+	int val;
+	struct Node *next;
+};
+
+#define SIZE 1000
+
+struct Node *HashTable[SIZE] = {NULL};
+
+int hash(int);
+void put(int);
+int get(int);
 int count_sub_array(int*, int, int);
-
+int* get_prefix_array(int*, int);
 
 int main() {
-	int arr[] = {2,3,1,2,4,3};
-    	int target = 7;
+	int arr[] = {1, 2, 3};
+	int k = 3;
 	int n = sizeof(arr) / sizeof(arr[0]);
-	printf("The number of substring with sum = %d is %d", target, count_sub_array(arr, n, target));
+	printf("The total amount of sub array = %d", count_sub_array(arr, n, k));
+	return 0;
 }
 
-int count_sub_array(int *arr, int size, int target) {
-	int current_sum = 0;
-	int count = 0;
+int* get_prefix_array(int *a, int size) {
+	int *prefix = (int*) malloc(sizeof(int) * size);
+	int sum = 0;
 	for (int i = 0; i < size; i++) {
-		current_sum += arr[i];
-		if (current_sum == target)
-			count++;
-		else if ((current_sum - target) == target)
-			count++;
+		sum += a[i];
+		prefix[i] = sum;
+	}
+	return prefix;
+}
+
+int hash(int key) {
+	return abs(key) % SIZE;
+}
+
+void put(int key) {
+	int index = hash(key);
+	struct Node *temp = HashTable[index];
+
+	while (temp != NULL) {
+		if (temp->key == key) {
+			temp->val++;
+			return;
+		}
+		temp = temp->next;
+	}
+	struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->key = key;
+        newNode->val = 1;
+	newNode->next = HashTable[index];
+	HashTable[index] = newNode;
+
+}
+
+int get(int key) {
+	int index = hash(key);
+	struct Node *temp = HashTable[index];
+
+	while (temp != NULL) {
+		if (temp->key == key) return temp->val;
+		temp = temp->next;
+	}
+	return 0;
+}
+
+
+int count_sub_array(int *a, int n, int k) {
+	int *prefix = get_prefix_array(a, n);
+	int count = 0;
+	put(0);
+	for (int i = 0; i < n; i++) {
+		count += get(prefix[i] - k);
+		put(prefix[i]);
 	}
 	return count;
-} 
+}
